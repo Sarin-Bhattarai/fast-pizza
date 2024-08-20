07890a314a9debb062f33f9f2a7caf68828bafba
sarin punk <sarin.bhattarai@icp.edu.np>
2024-08-20 10:01:43 +0545

    feat(outlet)

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,35 @@ import Home from "./components/Home";
 import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
+import AppLayout from "./components/AppLayout";
 import CreateOrder from "./features/order/CreateOrder";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Pizza Co.</Link>
+
+      <p>Sarin</p>
+    </header>
+  );
+}
+
+export default Header;
